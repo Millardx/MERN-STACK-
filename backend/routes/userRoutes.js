ca91e4fb2fb430adbb21b6788b5dcbe349c8d98d
Millardx <you@example.com>
2024-08-17 13:58:44 +0800

    MERN RBAC CRUD

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { getUsers, addUser, updateUser, deleteUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+// Route to get all users
+router.get('/all', getUsers);
+
+// Other routes (if any)
+router.post('/add', addUser);
+router.put('/update/:id', updateUser);
+router.delete('/delete/:id', deleteUser);
+
+module.exports = router;
